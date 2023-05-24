3b7b52dc97d7c1f0a85788abef12c9d9d77b3cc8
Nich H <nmjhorishny@gmail.com>
2023-05-24 15:49:58 +0100

    Swiper Pagination intergration

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,50 +1,151 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/swiper.min.css';
+import 'swiper/css/pagination';
 import './carousel.css';
+import github from '../assets/github.png';
 
-const Carousel = ({ images }) => {
-  const [currentImage, setCurrentImage] = useState(0);
+Swiper.use([Navigation, Pagination]);
 
-  const handlePrev = () => {
-    setCurrentImage((prevImage) =>
-      prevImage === 0 ? images.length - 1 : prevImage - 1
-    );
-  };
+const Carousel = () => {
+  useEffect(() => {
+    const swiper = new Swiper('.swiper', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 6,
+        slideShadows: true,
+      },
+      loop: true, // Enable continuous loop
+      freeMode: true, // Enable free dragging/swiping
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 2,
+        },
+        1560: {
+          slidesPerView: 2,
+        },
+      },
+    });
 
-  const handleNext = () => {
-    setCurrentImage((prevImage) =>
-      prevImage === images.length - 1 ? 0 : prevImage + 1
-    );
-  };
-
-  const handlePreviewClick = (index) => {
-    setCurrentImage(index);
-  };
+    return () => {
+      swiper.destroy();
+    };
+  }, []);
 
   return (
-    <div className="carousel">
-      <button className="carousel-button prev" onClick={handlePrev}>
-        &lt;
-      </button>
-      <img
-        className="carousel-image"
-        src={images[currentImage]}
-        alt={`Image ${currentImage}`}
-      />
-      <button className="carousel-button next" onClick={handleNext}>
-        &gt;
-      </button>
-      <div className="preview-grid">
-        {images.map((image, index) => (
-          <img
-            key={index}
-            className={`preview-image ${
-              index === currentImage ? 'active' : ''
-            }`}
-            src={image}
-            alt={`Image ${index}`}
-            onClick={() => handlePreviewClick(index)}
-          />
-        ))}
+    <div className="main">
+      <div>
+        <h1>projects</h1>
+        <p>
+          Beauty and mystery are hidden under the sea. Explore with
+          our application to know about Aquatic Animals.
+        </p>
+      </div>
+
+      <div className="swiper">
+        <div className="swiper-wrapper">
+          <div className="swiper-slide swiper-slide--one">
+            <div>
+              <h2>NFT Card</h2>
+              <p>About NFT Card</p>
+              <a href="https://en.wikipedia.org/wiki/Jellyfish">
+                <img
+                  className="carouselImage"
+                  src={github}
+                  href="https://en.wikipedia.org/wiki/Jellyfish"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  alt="githublink"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="swiper-slide swiper-slide--two">
+            <div>
+              <h2>RPS</h2>
+              <p>About RPS</p>
+              <a href="https://n335h.github.io/RPS/">
+                <img
+                  className="carouselImage"
+                  src={github}
+                  href="https://n335h.github.io/RPS/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  alt="githublink"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="swiper-slide swiper-slide--three">
+            <div>
+              <h2>Tesla Clone</h2>
+              <p>About Tesla Clone</p>
+              <a href="https://n335h.github.io/TeslaClone/">
+                <img
+                  className="carouselImage"
+                  src={github}
+                  href="https://n335h.github.io/TeslaClone/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  alt="githublink"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="swiper-slide swiper-slide--four">
+            <div>
+              <h2></h2>
+              <p></p>
+              <a href="">
+                <img
+                  className="carouselImage"
+                  src={github}
+                  href=""
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  alt="githublink"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="swiper-slide swiper-slide--five">
+            <div>
+              <h2> </h2>
+              <p></p>
+              <a href="">
+                <img
+                  className="carouselImage"
+                  src={github}
+                  href=""
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  alt="githublink"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="swiper-pagination"></div>
       </div>
     </div>
   );

4e932a8f02a31af2244d2545184a6657811203bd
Nich H <nmjhorishny@gmail.com>
2023-07-24 12:46:28 +0100

    form reset after send

--- src/components/pages/contact/contact.jsx
+++ src/components/pages/contact/contact.jsx
@@ -22,6 +22,7 @@ const Contact = () => {
         (result) => {
           console.log(result.text);
           console.log('success');
+          e.target.reset();
         },
         (error) => {
           console.log(error.text);

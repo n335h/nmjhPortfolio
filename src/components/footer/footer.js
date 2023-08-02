75adafecc83d2efe1303326407774b83691adcfc
Nich H <nmjhorishny@gmail.com>
2023-08-02 10:44:13 +0100

    resized slideinfo

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,18 @@
 import './styles.css';
 
-
 export default function Footer() {
   return (
     <div className="FooterContainer">
+      <div className="FooterTop">
+        <div className="FooterTitle">
+          <h1>footer!</h1>
+          <p> Footer Text</p>
+        </div>
+        <div className="FooterLinks">
+          <h1>footer links!</h1>
+          <p> Footer Links</p>
+        </div>
+      </div>
       <div className="Footer">
         <p> © Nich Horishny 2023</p>
       </div>

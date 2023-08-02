ca53f471eab26c7d400059068131ebccbabee928
Nich H <nmjhorishny@gmail.com>
2023-08-02 10:44:59 +0100

    Styles footer hidden

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,14 +4,14 @@ export default function Footer() {
   return (
     <div className="FooterContainer">
       <div className="FooterTop">
-        <div className="FooterTitle">
+        {/* <div className="FooterTitle">
           <h1>footer!</h1>
           <p> Footer Text</p>
         </div>
         <div className="FooterLinks">
           <h1>footer links!</h1>
           <p> Footer Links</p>
-        </div>
+        </div> */}
       </div>
       <div className="Footer">
         <p> © Nich Horishny 2023</p>

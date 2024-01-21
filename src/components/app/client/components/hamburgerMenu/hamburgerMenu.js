65b27597f1e60f17cdde1b1930237eddeaa64f14
Nich H <nmjhorishny@gmail.com>
2024-01-21 12:05:47 +0000

    menu

--- src/components/app/client/components/hamburgerMenu/hamburgerMenu.js
+++ src/components/app/client/components/hamburgerMenu/hamburgerMenu.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import './hamburger.css';
 
 const HamburgerMenu = ({ handleToggleMenu }) => {
-	return (
-		<div className='logo-toggle-container'>
-			<div
-				className='menu-toggle'
-				onClick={handleToggleMenu}>
-				<div className='bar h-0.5 mb-1 w-5 bg-indigo-600'></div>
-				<div className='bar h-0.5 mb-1 w-5 bg-indigo-600'></div>
-				<div className='bar h-0.5 mb-1 w-5 bg-indigo-600'></div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='logo-toggle-container'>
+      <div
+        className={`menu-toggle transition-transform duration-300 ${
+          handleToggleMenu ? 'open' : ''
+        }`}
+        onClick={handleToggleMenu}
+      >
+        <div className='bar h-0.5 mb-1 w-5 bg-indigo-600 transform transition-transform origin-center'></div>
+        <div className='bar h-0.5 mb-1 w-5 bg-indigo-600 transform transition-transform origin-center'></div>
+        <div className='bar h-0.5 mb-1 w-5 bg-indigo-600 transform transition-transform origin-center'></div>
+      </div>
+    </div>
+  );
 };
 
 export default HamburgerMenu;
